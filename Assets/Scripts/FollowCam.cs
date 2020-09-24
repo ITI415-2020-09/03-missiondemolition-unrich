ef85adfdd91f7f030488ee1b16d4035f85926d9b
unrich <60159029+unrich@users.noreply.github.com>
2020-09-24 16:48:53 -0400

    Set up clould, cloudAnchor, and #c cloud crafter

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -24,8 +24,8 @@ public class FollowCam : MonoBehaviour
     {
         if (poi == null) return;
         Vector3 destination = poi.transform.position;
-        destination.x = Mathf.Max(miniXY.x, destination.x);
-        destination.y = Mathf.Max(miniXY.y, destination.y);
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
